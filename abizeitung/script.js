a0131958d5a2a0ca3f1495cc424db821b5abec16
Lucadmin <luca.account@gmx.de>
2021-03-10 20:41:45 +0100

    Add: Form-Validation
    Signed-off-by: Lucadmin <luca.account@gmx.de>

--- abizeitung/script.js
+++ abizeitung/script.js
@@ -16,67 +16,68 @@
  */
 
 document.getElementById("submit-button").addEventListener("click", function (event) {
-    document.getElementById("loadingcircle").style.visibility = "visible";
-
-    event.preventDefault();
-    console.log("PDF-Datei wird erstellt")
-    const doc = new jsPDF();
-    doc.setFont("Montserrat-SemiBoldItalic", "bolditalic")
-    doc.setTextColor("#e0e0e0");
-    doc.setFontSize(15);
-    let pageHeight = doc.getPageHeight(0);
-    let pageWidth = doc.getPageWidth(0);
-    doc.addImage("hintergrund.png", 'PNG', 0, 0, pageWidth, pageHeight);
-    doc.text("Geburtstag: ", 30, 130)
-    doc.text("Lieblingsfach: ", 30, 140)
-    doc.text("Bester Lehrer: ", 30, 150)
-    if (document.getElementById("fdrink").value === "") {
-        doc.text("Meine Schullaufbahn als Filmtitel: ", 30, 160)
-        doc.text("Häufigster Abwesenheitsgrund: ", 30, 180)
-        doc.text("Was ich nach dem Abi machen werde: ", 30, 200)
-        doc.text("Lebensmotto: ", 30, 220)
-        doc.text("Was ich vermissen werde: ", 30, 240);
-    } else {
-        doc.text("Absturzgetränk Nr°1: ", 30, 160)
-        doc.text("Meine Schullaufbahn als Filmtitel: ", 30, 170)
-        doc.text("Häufigster Abwesenheitsgrund: ", 30, 190)
-        doc.text("Was ich nach dem Abi machen werde: ", 30, 210)
-        doc.text("Lebensmotto: ", 30, 230)
-        doc.text("Was ich vermissen werde: ", 30, 250);
-    }
-    //Handwriting
-    doc.setFont("Handlee", "normal")
-    doc.setFontSize(18);
-    doc.text(document.getElementById("fbday").value, 73, 130)
-    doc.text(document.getElementById("fach").value, 80, 140)
-    doc.text(document.getElementById("gender").value + " " + document.getElementById("fteachername").value, 80, 150)
-    if (document.getElementById("fdrink").value === "") {
-        doc.text(document.getElementById("ffilmtitel").value, 30, 160)
-        doc.text(document.getElementById("fabwesenheit").value, 30, 180)
-        doc.text(document.getElementById("fnachdemabi").value, 30, 200)
-        doc.text(document.getElementById("fmotto").value, 30, 220)
-        doc.text(document.getElementById("fvermissen").value, 30, 240);
-    } else {
-        doc.text(document.getElementById("fdrink").value, 100, 160)
-        doc.text(document.getElementById("ffilmtitel").value, 30, 180)
-        doc.text(document.getElementById("fabwesenheit").value, 30, 200)
-        doc.text(document.getElementById("fnachdemabi").value, 30, 220)
-        doc.text(document.getElementById("fmotto").value, 30, 240)
-        doc.text(document.getElementById("fvermissen").value, 30, 260);
-    }
-    doc.setFont("Montserrat-SemiBoldItalic", "bolditalic")
-    if (document.getElementById("fimagenew").files[0] !== undefined) {
-        let url = URL.createObjectURL(document.getElementById("fimageold").files[0]);
-        crop(url, 1).then((canvas) => {
-            doc.addImage(canvas, "PNG", 25, 25, 70, 70)
-            url = URL.createObjectURL(document.getElementById("fimagenew").files[0]);
+    if (document.getElementById("form").checkValidity()) {
+        document.getElementById("loadingcircle").style.visibility = "visible";
+        event.preventDefault();
+        console.log("PDF-Datei wird erstellt")
+        const doc = new jsPDF();
+        doc.setFont("Montserrat-SemiBoldItalic", "bolditalic")
+        doc.setTextColor("#e0e0e0");
+        doc.setFontSize(15);
+        let pageHeight = doc.getPageHeight(0);
+        let pageWidth = doc.getPageWidth(0);
+        doc.addImage("hintergrund.png", 'PNG', 0, 0, pageWidth, pageHeight);
+        doc.text("Geburtstag: ", 30, 130)
+        doc.text("Lieblingsfach: ", 30, 140)
+        doc.text("Bester Lehrer: ", 30, 150)
+        if (document.getElementById("fdrink").value === "") {
+            doc.text("Meine Schullaufbahn als Filmtitel: ", 30, 160)
+            doc.text("Häufigster Abwesenheitsgrund: ", 30, 180)
+            doc.text("Was ich nach dem Abi machen werde: ", 30, 200)
+            doc.text("Lebensmotto: ", 30, 220)
+            doc.text("Was ich vermissen werde: ", 30, 240);
+        } else {
+            doc.text("Absturzgetränk Nr°1: ", 30, 160)
+            doc.text("Meine Schullaufbahn als Filmtitel: ", 30, 170)
+            doc.text("Häufigster Abwesenheitsgrund: ", 30, 190)
+            doc.text("Was ich nach dem Abi machen werde: ", 30, 210)
+            doc.text("Lebensmotto: ", 30, 230)
+            doc.text("Was ich vermissen werde: ", 30, 250);
+        }
+        //Handwriting
+        doc.setFont("Handlee", "normal")
+        doc.setFontSize(18);
+        doc.text(document.getElementById("fbday").value, 73, 130)
+        doc.text(document.getElementById("fach").value, 80, 140)
+        doc.text(document.getElementById("gender").value + " " + document.getElementById("fteachername").value, 80, 150)
+        if (document.getElementById("fdrink").value === "") {
+            doc.text(document.getElementById("ffilmtitel").value, 30, 160)
+            doc.text(document.getElementById("fabwesenheit").value, 30, 180)
+            doc.text(document.getElementById("fnachdemabi").value, 30, 200)
+            doc.text(document.getElementById("fmotto").value, 30, 220)
+            doc.text(document.getElementById("fvermissen").value, 30, 240);
+        } else {
+            doc.text(document.getElementById("fdrink").value, 100, 160)
+            doc.text(document.getElementById("ffilmtitel").value, 30, 180)
+            doc.text(document.getElementById("fabwesenheit").value, 30, 200)
+            doc.text(document.getElementById("fnachdemabi").value, 30, 220)
+            doc.text(document.getElementById("fmotto").value, 30, 240)
+            doc.text(document.getElementById("fvermissen").value, 30, 260);
+        }
+        doc.setFont("Montserrat-SemiBoldItalic", "bolditalic")
+        if (document.getElementById("fimagenew").files[0] !== undefined) {
+            let url = URL.createObjectURL(document.getElementById("fimageold").files[0]);
             crop(url, 1).then((canvas) => {
-                doc.addImage(canvas, "PNG", 120, 25, 70, 70)
-                finishDocument(doc, pageWidth, pageHeight);
+                doc.addImage(canvas, "PNG", 25, 25, 70, 70)
+                url = URL.createObjectURL(document.getElementById("fimagenew").files[0]);
+                crop(url, 1).then((canvas) => {
+                    doc.addImage(canvas, "PNG", 120, 25, 70, 70)
+                    finishDocument(doc, pageWidth, pageHeight);
+                });
             });
-        });
-    } else {
-        finishDocument(doc, pageWidth, pageHeight);
+        } else {
+            finishDocument(doc, pageWidth, pageHeight);
+        }
     }
 })
 
