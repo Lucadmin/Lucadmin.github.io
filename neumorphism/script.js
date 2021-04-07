f8ee68c2eeb98008b67b8ebf62123ad6259a3f71
Lucadmin <luca.account@gmx.de>
2021-04-07 13:27:57 +0200

    Parcel introduced

--- neumorphism/script.js
+++
@@ -1,9 +0,0 @@
-button1 = document.getElementById("button1")
-
-button1.addEventListener("click", function () {
-    if (button1.className === "test1") {
-        button1.className = "test1 test1--pressed"
-    } else {
-        button1.className = "test1"
-    }
-})
